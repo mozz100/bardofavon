b6f29e809cbfb75408762105985ed9b6c4f39470
Richard Morrison <richard@rmorrison.net>
2012-04-25 22:10:12 +0100

    Add some analytics to dramatis personae

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -3,6 +3,22 @@ class CharactersController < ApplicationController
 
   def index
     @characters = @work.characters.order('name').where('abbrev NOT IN (?)', ['All', 'xxx'])   #  should be in order of appearance... query challenge!
+
+    # build array of characters with name/abbrev and paragraph count for chart
+    # This could be more efficient: it'll hit the db for each character to call SELECT COUNT(*) FROM paragraphs...
+    # But hey, Shakespeare wouldn't mind.
+    
+    @pie_chart = @characters.map do |c|
+      {
+        :name  => c.name,
+        :pcount => c.paragraphs.where(:chapter_id => @work.chapters.map(&:id)).count
+      }
+    end
+
+    # sort by paragraph count DESC
+    @pie_chart.sort! do |e1, e2|
+      e2[:pcount] <=> e1[:pcount]
+    end
   end
 
   private
